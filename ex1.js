a5522d959f50c22c966b083c15c2dfb8eede3ad8
mateusfonseca <mateusff8@gmail.com>
2025-09-30 15:40:08 +0100

    Added remove weight button and remove weight function.

--- ex1.js
+++ ex1.js
@@ -1,12 +1,26 @@
-function checkBaggageWeight(baggageWeight) {
+function checkBaggageWeight(weight) {
     switch(true) {
-        case isNaN(baggageWeight):
-            alert(`Something went wrong. Please, try inputing the baggage weight again.`)
+        case isNaN(weight):
+            document.getElementById("message").textContent = `Something went wrong. Please, try again.`
             break
-        case baggageWeight > 15:
-            alert(`Baggage weight is ${baggageWeight} kg and must be checked!`)
+        case weight > 15:
+            document.getElementById("message").textContent = `Baggage weight is ${weight} kg. Please, remove some weight.`
+            document.getElementById("remove").removeAttribute("hidden")
             break
         default:
-            alert(`Baggage weight is ${baggageWeight} kg. All clear!`)
+            document.getElementById("message").textContent = `Baggage weight is ${weight} kg. All clear.`
+    }
+}
+
+function removeSomeWeight(weight) {
+    switch(true) {
+        case isNaN(weight):
+            document.getElementById("message").textContent = `Something went wrong. Please, try again.`
+            break
+        default:
+            let newWeight = document.getElementById("baggageWeight").value - weight           
+            if (newWeight < 0) newWeight = 0
+            document.getElementById("baggageWeight").value = newWeight
+            checkBaggageWeight(newWeight)
     }
 }

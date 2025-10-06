7bdaa0f1f4c8dc72f2ec28088a432d5859018b4e
mateusfonseca <mateusff8@gmail.com>
2025-10-06 10:37:36 +0100

    Moved business logic out and kept only UI operations here.

--- js/ex3.js
+++ js/ex3.js
@@ -1,21 +1,8 @@
-function listEuler1(a, b, l) {
-    sum = l.
-        filter(x => x % a == 0 || x % b == 0).
-        reduce((sum, x) => sum + x, 0)
+// ideally would be imported from a business rules script, but cannot be done without a server due to CORS violation. Solution was to link businessRules.js to the HTML directly.
+// import {euler} from "./businessRules"
 
-    alert(`The sum of all the multiples of ${a} or ${b} in ${l.slice(0, l.length-1).join(", ") + " and " + l[l.length-1]} is ${sum}.`)
-}
+let listEuler1 = (a, b, l) => alert(`The sum of all the multiples of ${a} or ${b} in ${l.slice(0, l.length-1).join(", ") + " and " + l[l.length-1]} is ${euler([a, b], l)}.`)
 
-function listEuler2(a, l) {
-    inputAEx3B.value = a
-    listEuler3(a, l)
-}
+let listEuler2 = (a, l) => {inputAEx3B.value = a; listEuler3(a, l)}
 
-function listEuler3(a, l) {
-    sum = a.
-        map(x => l.filter(y => y % x == 0)).
-        flat().
-        reduce((sum, x) => sum + x, 0)
-
-    alert(`The sum of all the numbers in ${l.slice(0, l.length-1).join(", ") + " and " + l[l.length-1]} that are multiples of numbers in ${a.slice(0, a.length-1).join(", ") + " and " + a[a.length-1]} is ${sum}.`)
-}
+let listEuler3 = (a, l) => alert(`The sum of all the numbers in ${l.slice(0, l.length-1).join(", ") + " and " + l[l.length-1]} that are multiples of numbers in ${a.slice(0, a.length-1).join(", ") + " and " + a[a.length-1]} is ${euler(a, l)}.`)

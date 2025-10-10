b2061c1421d0ee2c2c104bcdedab009ee71d655e
mateusfonseca <mateusff8@gmail.com>
2025-10-10 21:04:48 +0100

    parseInputs, euler and prettyList to be imported when running on a server.

--- js/ex2.js
+++ js/ex2.js
@@ -1,4 +1,11 @@
-function euler1(a = 3, b = 5, n = 1000) {
+// ideally would be imported from a business rules script, but cannot be done without a server due to CORS violation. Solution was to link businessRules.js to the HTML directly.
+// import {parseInputs, euler, prettyList} from "./businessRules"
+
+function euler2a(a = 3, b = 5, n = 1000) {
+    
+    /*
+    procedural version (original attempt):
+    
     let sum = 0
     for (let i = 1; i < n; i++) {
         if (i % a == 0 || i % b == 0) {

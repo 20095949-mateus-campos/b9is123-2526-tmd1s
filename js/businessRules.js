449d7feb9b20a4fe4ae3cac6656a409634264318
mateusfonseca <mateusff8@gmail.com>
2025-10-10 21:02:18 +0100

    Added error throwing to euler function and prettyList function to businessRules.

--- js/businessRules.js
+++ js/businessRules.js
@@ -6,10 +6,22 @@ function parseInputs(inputs) {
 }
 
 function euler(factors, multiples) {
-    return factors.
-            map(factor => multiples.filter(multiple => multiple % factor == 0)).
-            flat().
-            reduce((sum, factor) => sum + factor, 0)
+    if (factors.some(isNaN) || multiples.some(isNaN)) {
+        throw TypeError("Expected list of integers. Got NaN.")
+    }
+
+    if (!factors.length || !multiples.length) {
+        throw RangeError("Expected list of integers. Got empty list.")
+    }
+
+    return factors
+        .map(factor => multiples.filter(multiple => multiple % factor == 0))
+        .flat()
+        .reduce((sum, factor) => sum + factor, 0)
+}
+
+function prettyList(list) {
+    return list.slice(0, list.length - 1).join(", ") + " and " + list[list.length - 1]
 }
 
 // ideally would be exported as modules, but cannot be done without a server due to CORS violation. Solution was to link businessRules.js to the HTML directly.
